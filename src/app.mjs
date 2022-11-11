d4bede2478b84bc8c7e0333d2de21f0d03f03c11
Viki Val <viktorko99@gmail.com>
2022-11-11 19:11:45 +0100

    :zap: ability to fetch image

--- src/app.mjs
+++ src/app.mjs
@@ -11,15 +11,14 @@ app.post('/border', async ({ body }, res) => {
   // const collection = createCollection('HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F', 'CHNK', 'Chunkies', 'ipfs://ipfs/bafkreibzeyetfssguxrzoltvluyjac7hp3bzvzgpa27jkbpek23tqkfpmi', 10000)
   const { url } = body
   const input = await $fetch(url, { responseType: 'blob' })
-  const buffer = sharp(input)
-  .resize(180)
-  .background({r: 100, g: 0, b: 0})
-  .extend({top: 10, left: 10, bottom: 20, right: 10})
-  .toBuffer()
+  const magic = await input.arrayBuffer()
+  const view = new Uint8Array(magic);
+  const buffer = await sharp(view).metadata();
   
   // send that as image to the client
-  res.type('image/png')
-  res.send(buffer)
+  // res.type('image/png')
+  // res.send(buffer)
+  res.json(buffer)
 })
 
 
