58e585333adec438522b6838034037d8a5e1013c
Viki Val <viktorko99@gmail.com>
2022-11-11 20:37:11 +0100

    :zap: I have MVP

--- src/app.mjs
+++ src/app.mjs
@@ -7,20 +7,55 @@ app.use(express.json())
 
 const port = 3000
 
+const svgBuffer = (text = '1') => {
+  const sn = text.padStart(4, '0')
+  const magic = `
+  <svg width="130" height="70">
+  <style>
+  .title { font-size: 43px; font-weight: bold; font-family: sans-serif; fill: #fff; }
+  </style>
+  <text x="50%" y="50%" text-anchor="middle" class="title">#${sn}</text>
+</svg>
+`
+
+  return Buffer.from(magic)
+}
+
 app.post('/border', async ({ body }, res) => {
-  // const collection = createCollection('HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F', 'CHNK', 'Chunkies', 'ipfs://ipfs/bafkreibzeyetfssguxrzoltvluyjac7hp3bzvzgpa27jkbpek23tqkfpmi', 10000)
   const { url } = body
   const input = await $fetch(url, { responseType: 'blob' })
   const magic = await input.arrayBuffer()
-  const view = new Uint8Array(magic);
-  const buffer = await sharp(view).metadata();
-  
+  const view = new Uint8Array(magic)
+  const data = await sharp(view)
+    .resize(512, 512)
+    .extend({
+      bottom: 70,
+      background: { r: 0, g: 0, b: 0, alpha: 0.1 },
+    })
+    .composite([
+      {
+        input: 'kodadot.svg',
+        top: 522,
+        left: 10,
+      },
+      {
+        input: 'sub0-1.svg',
+        top: 522,
+        left: 200,
+      },
+      {
+        input: svgBuffer('2'),
+        top: 522,
+        left: 372,
+      },
+    ])
+    .toBuffer()
+
   // send that as image to the client
   // res.type('image/png')
   // res.send(buffer)
-  res.json(buffer)
+  res.type('png').send(data)
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
